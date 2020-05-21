a9fab1ac268f3677996df569de9be171e7560854
Halim <wngkfla01@gmail.com>
2020-05-21 12:21:03 +0900

    Java Ch15

---
+++ Ch15/src/sub2/TreeSetTest.java
@@ -0,0 +1,32 @@
+package sub2;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+ * 날짜 : 2020/05/20
+ * 이름 : 주하림
+ * 내용 : TreeSet 실습하기 교재 p752
+ */
+
+public class TreeSetTest {
+	public static void main(String[] args) {
+		
+		// TreeSet 생성 및 숫자 데이터 저장
+		Set<Integer> tree = new TreeSet<>();
+		tree.add(23);
+		tree.add(10);
+		tree.add(48);
+		tree.add(15);
+		tree.add(7);
+		tree.add(22);
+		tree.add(56);
+		
+		for (int num : tree) {
+			System.out.println(num+", ");
+			
+		}
+		
+	}
+
+}
